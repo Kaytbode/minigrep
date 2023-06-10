3af7ee57db0c64a7b5c4240cb2d39463ff9b9eef
Kayode <bkaoluborode@gmail.com>
2023-06-10 11:14:05 +0100

    feat: read command-line args and collects them

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // read any command line arguments passed to it 
+    // and then collect the values into a vector.
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+    dbg!(args);
 }
